f46dd347440393b52874fa791a0352750c07fda3
GabriellMiranda <gabriel2020miranda@gmail.com>
2023-01-04 11:08:34 -0300

    validacao de dados nacionais concluida

---
+++ 5_validacao_de_dados/acesso_cep.py
@@ -0,0 +1,36 @@
+import requests
+class SearchAdress:
+
+    def __init__(self, cep:str) -> None:
+        if self.cep_is_valid(cep):
+            self.cep = cep
+        else:
+            raise ValueError("Cep invalid")
+
+
+    def cep_is_valid(self, cep:str) -> bool:
+        if len(cep) == 8:
+            return True
+        else:
+            return False
+    
+    def __str__(self) -> str:
+        return f'{self.cep[:5]}-{self.cep[5:]}'
+
+
+    def acess_by_cep(self):
+        url = f'https://viacep.com.br/ws/{self.cep}/json/'
+        r = requests.get(url)
+        dados = r.json()
+        return (
+            dados['bairro'],
+            dados['localidade'],
+            dados['uf']
+        )
+
+
+if __name__=='__main__':
+    cep = '01001000'
+    obj_cep = SearchAdress(cep)
+    print(obj_cep.acess_by_cep())
+    
