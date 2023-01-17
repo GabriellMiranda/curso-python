0869a1735692369a2736f47ba6d6622af0e25a67
GabriellMiranda <gabriel2020miranda@gmail.com>
2023-01-17 15:24:43 -0300

    init testes automatizados

--- _8_testes_altomatizados/src/leilao/dominio.py
+++ _8_testes_altomatizados/src/leilao/dominio.py
@@ -1,26 +1,41 @@
+import sys
+
 class Usuario:
 
-    def __init__(self, nome):
+    def __init__(self, nome: str) -> None:
         self.__nome = nome
 
     @property
-    def nome(self):
+    def nome(self) -> str:
         return self.__nome
 
 
 class Lance:
 
-    def __init__(self, usuario, valor):
+    def __init__(self, usuario: str, valor: float) -> None:
         self.usuario = usuario
         self.valor = valor
 
 
 class Leilao:
 
-    def __init__(self, descricao):
+    def __init__(self, descricao: str) -> None:
         self.descricao = descricao
         self.__lances = []
 
     @property
-    def lances(self):
+    def lances(self) -> str:
         return self.__lances
+
+class Avaliador:
+
+    def __init__(self) -> None:
+        self.maior_lance = sys.float_info.min
+        self.menor_lance = sys.float_info.max
+
+    def avalia(self, leilao: Leilao) -> None:
+        for lance in leilao.lances:
+            if lance.valor > self.maior_lance:
+                self.maior_lance = lance.valor
+            if lance.valor < self.menor_lance:
+                self.menor_lance = lance.valor
